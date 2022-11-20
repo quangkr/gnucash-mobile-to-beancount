ee963c700116e1ec3ecc8ed11583dae984672734
Quang KR <dangminhquang.r@gmail.com>
2022-11-20 20:07:15 +0700

    [Script] Get paths from within python script

--- convert.py
+++ convert.py
@@ -1,8 +1,46 @@
 #!/usr/bin/env python3
 
-import sys
 import csv
 import unicodedata
+from pathlib import Path
+
+
+def main():
+    script_path = Path(__file__).absolute().parent
+    input_path = sorted(script_path.parent.joinpath('gnucash').iterdir(), key=lambda f: f.stat().st_mtime, reverse=True)[0]
+    template_path = script_path.joinpath('template.beancount')
+    output_path = script_path.joinpath('output.beancount')
+    with input_path.open(mode='r', newline='') as in_f, template_path.open(mode='r') as tem_f, output_path.open(mode='w') as out_f:
+        # copy template over to the output
+        out_f.writelines(tem_f)
+        # read input CSV, then transform the data
+        # and write directly to the output
+        transform_csv(in_f, out_f)
+    # start fava
+    #start_fava(output_path)
+
+
+def start_fava(path):
+    try:
+        from fava.application import app
+        app.config['BEANCOUNT_FILES'] = [path]
+        app.run('localhost', 5000)
+    except ModuleNotFoundError:
+        print('Unable to import module fava')
+
+
+def transform_csv(input_file, output_file):
+    reader = csv.DictReader(input_file)
+    currency = ''
+    for row in reader:
+        if row['Date']:
+            output_file.write('{} * "{}"\n'.format(row['Date'], row['Description']))
+            currency = row['Commodity/Currency'].split(':')[-1]
+
+        account = row['Full Account Name'].replace(' ', '-').replace(',', '')
+        amount = row['Amount Num']
+        output_file.write('  {}        {} {}\n'.format(
+            account, amount, currency))
 
 
 # Unused for now
@@ -11,18 +49,5 @@ def strip_accents(s):
                    if unicodedata.category(c) != 'Mn')
 
 
-with open(sys.argv[1], mode='r', newline='') as in_f, open('template.beancount', mode='r') as tem_f, open('output.beancount', mode='w') as out_f:
-    # copy template over to the output
-    out_f.writelines(tem_f)
-
-    # read input CSV, then transform the data and write directly to the output
-    reader = csv.DictReader(in_f)
-    currency = ''
-    for row in reader:
-        if row['Date']:
-            out_f.write('{} * "{}"\n'.format(row['Date'], row['Description']))
-            currency = row['Commodity/Currency'].split(':')[-1]
-
-        account = row['Full Account Name'].replace(' ', '-').replace(',', '')
-        out_f.write('  {}        {} {}\n'.format(
-            account, row['Amount Num'], currency))
+if __name__ == "__main__":
+    main()
--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 DIR=$(dirname $(realpath "${0}"))
-INPUT_DIR='../gnucash'
-INPUT_FILE=$(realpath $(ls "${INPUT_DIR}"/*.csv -At | head -n1))
-
-$DIR/convert.py "${INPUT_FILE}" && fava output.beancount
+$DIR/convert.py && fava "${DIR}/output.beancount"
