74621e7c57283e78b6c7dfaed589ff9c8d27ea69
Quang KR <dangminhquang.r@gmail.com>
2023-08-23 17:12:38 +0700

    [Script] Handle CSV exported from GnuCash Desktop

--- convert.py
+++ convert.py
@@ -7,8 +7,8 @@ from pathlib import Path
 
 def main():
     script_path = Path(__file__).absolute().parent
-    input_path = sorted(script_path.parent.joinpath(
-        'gnucash').iterdir(), key=lambda f: f.stat().st_mtime, reverse=True)[0]
+    input_path = max(script_path.parent.joinpath(
+        'gnucash').glob(r'*.csv'), key=lambda f: f.stat().st_mtime)
     template_path = script_path.joinpath('template.beancount')
     output_path = script_path.joinpath('output.beancount')
     with input_path.open(mode='r', newline='') as in_f, template_path.open(mode='r') as tem_f, output_path.open(mode='w') as out_f:
@@ -36,14 +36,19 @@ def start_fava(path):
 def transform_csv(input_file, output_file):
     reader = csv.DictReader(input_file)
     currency = ''
+    transaction_id = ''
+    header_amount = 'Amount Num'
+    if (header_amount not in reader.fieldnames):
+        header_amount = 'Amount Num.'
     for row in reader:
-        if row['Date']:
+        if (row['Transaction ID'] and row['Transaction ID'] != transaction_id):
+            transaction_id = row['Transaction ID']
             output_file.write(
-                '{} * "{}"\n'.format(row['Date'], row['Description']))
+                    '{} * "{}"\n'.format(row['Date'], row['Description']))
             currency = row['Commodity/Currency'].split(':')[-1]
 
         account = row['Full Account Name'].replace(' ', '-').replace(',', '')
-        amount = row['Amount Num']
+        amount = row[header_amount].replace('(', '-').replace(')', '')
         output_file.write('  {}        {} {}\n'.format(
             account, amount, currency))
 
