a84ea58dc7041f70839070b07296350c2e39f0b0
Quang KR <dangminhquang.r@gmail.com>
2023-08-24 12:31:37 +0700

    Allow .gnca, .gnucash as input file extension

--- convert.py
+++ convert.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-import csv
-import unicodedata
 from pathlib import Path
+from itertools import chain
 
 
 def main():
     script_path = Path(__file__).absolute().parent
-    input_path = max(script_path.parent.joinpath(
-        'gnucash').glob(r'*.csv'), key=lambda f: f.stat().st_mtime)
     template_path = script_path.joinpath('template.beancount')
     output_path = script_path.joinpath('output.beancount')
+
+    extensions = { '.csv', '.gnca', '.gnucash' }
+    files = chain(script_path.parent.glob('gnucash/*'), script_path.glob('gnucash/*'))
+    filtered_files = filter(lambda f: f.suffix in extensions, files)
+    input_path = max(filtered_files, key=lambda f: f.stat().st_mtime)
     with input_path.open(mode='r', newline='') as in_f, template_path.open(mode='r') as tem_f, output_path.open(mode='w') as out_f:
         # copy template over to the output
         out_f.writelines(tem_f)
         # read input CSV, then transform the data
         # and write directly to the output
-        transform_csv(in_f, out_f)
+        if input_path.suffix in { '.csv' }:
+            transform_csv(in_f, out_f)
     # start fava
     start_fava(output_path)
 
@@ -34,29 +37,29 @@ def start_fava(path):
 
 
 def transform_csv(input_file, output_file):
+    import csv
     reader = csv.DictReader(input_file)
     currency = ''
     transaction_id = ''
+    # gnucash desktop has different header name
     header_amount = 'Amount Num'
     if (header_amount not in reader.fieldnames):
         header_amount = 'Amount Num.'
     for row in reader:
         if (row['Transaction ID'] and row['Transaction ID'] != transaction_id):
             transaction_id = row['Transaction ID']
-            output_file.write(
-                    '{} * "{}"\n'.format(row['Date'], row['Description']))
+            output_file.write('{} * "{}"\n'.format(row['Date'], row['Description']))
             currency = row['Commodity/Currency'].split(':')[-1]
-
+        # gnucash desktop use parentheses for negative number
         account = row['Full Account Name'].replace(' ', '-').replace(',', '')
         amount = row[header_amount].replace('(', '-').replace(')', '')
-        output_file.write('  {}        {} {}\n'.format(
-            account, amount, currency))
+        output_file.write('  {}        {} {}\n'.format(account, amount, currency))
 
 
 # Unused for now
 def strip_accents(s):
-    return ''.join(c for c in unicodedata.normalize('NFD', s)
-                   if unicodedata.category(c) != 'Mn')
+    import unicodedata
+    return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
 
 
 if __name__ == "__main__":
